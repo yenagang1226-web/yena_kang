7f3fe238dca6e8df781eaff8fe8786892d193406
yenagang1226-web <yenagang1226@gmail.com>
2026-05-19 17:48:06 +0900

    Update biz_st.py

--- biz_st.py
+++ biz_st.py
@@ -1,6 +1,13 @@
 import streamlit as st
 st.title('첫번재 웹 어플 만들기')
 
-"## 이건 부제목"
+"# 첫번재 웹 어플 만들기"
+"###### 첫번째 웹 어플 만들기"
 
-"이건 목록"
+'''### Magic 적용
+1. ordered item
+    - 강조: **unordered item**
+    - 기울임: *unordered item*
+2. ordered item
+3. ordered item
+'''
