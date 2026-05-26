a0c689fb0edee70777300ce7414d85a6154c6b32
yenagang1226-web <yenagang1226@gmail.com>
2026-05-26 15:24:25 +0900

    Update biz_st.py

--- biz_st.py
+++ biz_st.py
@@ -1,13 +1,6 @@
 import streamlit as st
-st.title('첫번재 웹 어플 만들기')
 
-"# 첫번재 웹 어플 만들기"
-"###### 첫번째 웹 어플 만들기"
-
-'''### Magic 적용
-1. ordered item
-    - 강조: **unordered item**
-    - 기울임: *unordered item*
-2. ordered item
-3. ordered item
-'''
+''''''
+# 비즈니스 모델 분석
+''''''
+[네이버](https://www.naver.com)
