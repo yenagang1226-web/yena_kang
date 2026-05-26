3f6bc305c45d38ed01878c23977b4ac2919321f8
yenagang1226-web <yenagang1226@gmail.com>
2026-05-26 15:37:30 +0900

    Update biz_st.py

--- biz_st.py
+++ biz_st.py
@@ -1,9 +1,16 @@
 import streamlit as st
 
 # 비즈니스 모델 분석
+st.title("비즈니스 모델 분석")
 
 st.markdown("[네이버](https://www.naver.com)")
+st.markdown("[홍익대학교](https://www.hongik.ac.kr)")
 
-이것이 일반 본문
+st.write("이것이 일반 본문")
 
-st.caption('캡션(작고흐린글씨로 표현됨):st.caption()')
+st.caption("캡션: 작고 흐린 글씨로 표현됨")
+
+with st.echo():
+    # 이 블록의 코드와 결과를 출력
+    name = "Yena Kang"
+    st.write("Hello, Streamlit!", name)
