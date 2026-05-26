0a67b40cdb7eac27965f132762532c605b57bebf
yenagang1226-web <yenagang1226@gmail.com>
2026-05-26 16:19:06 +0900

    Update biz_st.py

--- biz_st.py
+++ biz_st.py
@@ -17,13 +17,13 @@ with st.echo():
 
 # 🎥 : 이미지, 오디오, 동영상
 
-'### :orange[이미지: st.image()]'
-st.image("./AHFFK/파이썬 사진.webp", caption="파이썬 로고", width=500)
+st.title("비즈니스 모델 분석")
 
-'### :orange[오디오: st.audio()]'
-st.audio("./data/After_You.mp3", format="audio/mpeg", loop=True)
+st.markdown("### :orange[이미지: st.image()]")
+st.image("파이썬 사진.webp", caption="파이썬 로고", width=500)
 
-'### :orange[동영상: st.video()]'
+st.markdown("### :orange[오디오: st.audio()]")
+st.audio("성당.mp3", format="audio/mpeg", loop=True)
 
 # 'rb' : 바이너리 모드로 파일 열기
 video_file = open("./data/stars.mp4", "rb")
