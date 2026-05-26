293c6feb884e60e91e3daa0848d194a3a3657849
yenagang1226-web <yenagang1226@gmail.com>
2026-05-26 16:50:46 +0900

    Update biz_st.py

--- biz_st.py
+++ biz_st.py
@@ -43,7 +43,7 @@ st.markdown("### :orange[지표(Metric)]")
 col1, col2, col3 = st.columns(3)
 
 col1.metric("Temperature", "70 °F", "1.2 °F")
-col1.wirte("이것은 온도 지표입니다.")
+col1.write("이것은 온도 지표입니다.")
 col2.metric("Wind", "9 mph", "-8%")
 col3.metric("Humidity", "86%", "4%")
 
